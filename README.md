9f741ae5d85ee47f2f7e669870ca57f82d5f0cee
MARCHAL Simon <marchal.simon@gmail.com>
2020-05-18 13:01:37 +0200

    Cleaning to start from scratch

--- README.md
+++
@@ -1,7 +0,0 @@
-# config.json
-
-```json
-{
-  "token": "ChangeWithYourDiscordBotToken"
-}
-```
--- events/ready.js
+++
@@ -1,3 +0,0 @@
-module.exports = () => {
-    console.log(`${module.parent.exports.user.username} ready to play!`);
-}
