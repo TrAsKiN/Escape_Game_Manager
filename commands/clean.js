0f915de324b338580444a3fce51714449b0dc185
MARCHAL Simon <marchal.simon@gmail.com>
2020-05-18 17:21:12 +0200

    Removal of mandatory argument

--- commands/clean.js
+++ commands/clean.js
@@ -1,15 +1,14 @@
 module.exports = {
     name: 'clean',
     description: 'Allows you to delete between 1 and 99 old messages.',
-    args: true,
     usage: '<number>',
     guildOnly: true,
     aliases: ['clear'],
     execute(message, args) {
-        const amount = parseInt(args[0]) + 1;
+        let amount = parseInt(args[0]) + 1;
 
         if (isNaN(amount)) {
-            return message.reply(`that doesn't seem to be a valid number.`);
+            amount = 100;
         } else if (amount <= 1 || amount > 100) {
             return message.reply(`you need to input a number between 1 and 99.`);
         }
