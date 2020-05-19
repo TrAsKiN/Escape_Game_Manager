fce0442fe27c70df1b42a01388948235a80b3ee0
MARCHAL Simon <marchal.simon@gmail.com>
2020-05-19 12:38:28 +0200

    Creating the config.json file after installation

---
+++ install.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+fs.access('config.json', fs.constants.F_OK, (doesNotExist) => {
+    if (doesNotExist) {
+        rl.question(`Enter your bot token: `, (answer) => {
+            rl.close();
+
+            const config = {
+                "prefix": "!",
+                "token": `${answer}`
+            };
+            
+            fs.writeFile('config.json', JSON.stringify(config), 'utf8', (error) => {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log(`The config.json file has been created!\n`);
+                }
+            });
+        });
+    } else {
+        rl.close();
+    }
+});
